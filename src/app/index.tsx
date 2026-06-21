f0ef4e4ff6538f5603c1ba819675427bd7c8cf1c
Matt Elcock <matthew.elcock82@googlemail.com>
2026-06-21 16:30:50 +0100

    chore(no-linear): collapse features dir
    It wasn't needed in the end. Opting instead for having UI on the page itself.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,31 @@
-import { YourGardens } from '@/features/YourGardens/YourGardens';
-import { View } from 'react-native';
+import { headings } from '@/design-system/styles/headings';
+import { useRouter } from 'expo-router';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
+  const router = useRouter();
+
+  const onAddGardenClick = () => {
+    router.push('/create-garden');
+  };
+
   return (
     <View>
-      <YourGardens />
+      <View style={styles.header}>
+        <Text style={styles.heading}>My gardens</Text>
+        <Button title="Add garden" onPress={onAddGardenClick} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  heading: {
+    fontSize: headings.h2,
+  },
+});
